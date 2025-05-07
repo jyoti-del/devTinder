74b47e41f7bc9edd14ff8d4668747a898f7468b4
Jyoti <64889330+jyoti-del@users.noreply.github.com>
2025-05-07 23:11:04 +0530

    crud api

--- src/app.js
+++ src/app.js
@@ -5,13 +5,10 @@ const app = express();
 
 const port = 7777
 
+app.use(express.json())
+
 app.post('/signup' , async (req , res) =>{
-    const user = new User({
-        firstName : "Ujjwal",
-        lastName  : "Bhardwaj",
-        emailId : "ujjwal.bhardwaj@gmail.com",
-        password : '123'
-    })
+    const user = new User(req.body)
 
     try{
     await user.save()
@@ -20,6 +17,74 @@ app.post('/signup' , async (req , res) =>{
         res.status(400).send("Error data saving" + err.message)
     }
 })
+
+// get user by emailId
+app.get('/user' , async (req , res) =>{
+    const userEmail = req.body.emailId
+    try{
+       const user = await User.findOne({emailId : userEmail })
+       if(!user){
+         res.status(404).send("User Not Found")
+
+    // const user = await User.find({emailId : userEmail})
+    // if(user.length == 0){
+    //     res.status(404).send("User Not Found")
+       }else{
+        res.send(user)
+       }
+    }catch(err){
+        res.status(400).send("Something went wrong")
+    }
+}) 
+
+
+// get all users   --- feed 
+app.get('/feed' , async(req , res)=>{
+    try{
+    const allUser = await User.find({})
+    res.send(allUser)
+    }catch(err){
+        res.send(400).send("Something went wrong")
+    }
+})
+
+// delete user 
+app.delete("/user" , async (req , res) =>{
+    const userId = req.body.userId
+    try{
+    //   const result = await User.findByIdAndDelete(userId);
+    const result = await User.findByIdAndDelete({_id : userId});
+       res.send("User deleted successfully")
+    }catch(err){
+        res.status(400).send("Something went wrong")
+    }
+})
+
+// update user by id
+app.patch('/user' , async(req , res)=>{
+    const userId = req.body.userId;
+    const data = req.body;
+    try{
+      await User.findByIdAndUpdate(userId , data);
+      res.send("User updated successfully")
+    }catch(err){
+        res.status(400).send("Something went wrong")
+    }
+})
+
+// update user by email
+app.patch('/userEmail' , async(req , res) =>{
+    const userEmail = req.body.emailId;
+    const data = req.body;
+    try{
+     await User.findOneAndUpdate({emailId : userEmail} , data)
+     res.send("User with email updated successfully")
+    }catch(err){
+        res.status(400).send("Something went wrong")
+    }
+})
+
+
 connectDB().then(() =>{
   console.log("Database connected successfully")
   app.listen(port , () =>{
